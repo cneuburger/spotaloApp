a9924e2682b5888f0af815529292fe118cb565a1
Christian Neuburger <neibsi@gmail.com>
2025-08-11 06:11:16 +0200

    added location service ... leaflet

---
+++ src/app/services/location.service.ts
@@ -0,0 +1,104 @@
+import { Injectable } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { Geolocation } from '@capacitor/geolocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationService {
+
+  locationData: any = {
+    lat: 0,
+    lng: 0,
+    ready: false
+  };
+
+  constructor() { }
+
+
+  async initGeoLocation(): Promise<void> {
+    try {
+      if (Capacitor.isNativePlatform()) {
+        // Anfrage der Berechtigung zur Laufzeit auf mobilen Geräten
+        const permStatus = await Geolocation.checkPermissions();
+
+        if (permStatus.location !== 'granted') {
+          const newPerm = await Geolocation.requestPermissions();
+          if (newPerm.location !== 'granted') {
+            throw new Error('Location permission not granted');
+          }
+        }
+      }
+
+      const position = await Geolocation.getCurrentPosition();
+
+      this.locationData = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      console.log('position: ', JSON.stringify(this.locationData));
+    } catch (error) {
+      console.error('Fehler beim Abrufen der Geoposition:', error);
+      // Optional: Fallback-Location setzen
+      this.locationData = {
+        lat: 51.5074, // London
+        lng: -0.1278
+      };
+    }
+  }
+
+
+  initGeoLocationBrowser(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log('Browser position:', position);
+          this.locationData = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+            ready: true
+          };
+          resolve();
+        },
+        error => {
+          console.error('Error getting location from browser:', error);
+          reject(error);
+        }
+      );
+    });
+  }
+
+
+  async initGeoLocationDevice() {
+
+    this.locationData.lat = 0;
+    this.locationData.lng = 0;
+    this.locationData.ready = false;
+
+
+    // Use Capacitor's plugin on native platforms
+    try {
+      const perm = await Geolocation.requestPermissions();
+      if (perm.location === 'granted') {
+        console.log('granted!');
+
+        const position = await Geolocation.getCurrentPosition({
+          enableHighAccuracy: true,
+          timeout: 15000,
+        });
+
+        console.log('Position:', position);
+        
+        this.locationData.lat = position.coords.latitude;
+        this.locationData.lng = position.coords.longitude;
+        this.locationData.ready = true;
+
+      } else {
+        console.error('Keine Berechtigung für Standort');
+      }
+    } catch (error) {
+      console.error('Error getting location from Capacitor:', error);
+    }
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
+import * as L from 'leaflet';
+import { LocationService } from '../services/location.service';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -9,5 +12,90 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
+
+  error: string | null = null;
+  map!: L.Map;
+  marker!: L.Marker;
+
+  constructor(private locationService: LocationService) {}
+
+
+  async ionViewDidEnter() {
+    await this.locationService.initGeoLocation();
+
+    const loc = this.locationService.locationData;
+    if (loc) {
+      console.log('position in tab1', JSON.stringify(loc));
+      this.showMap(loc.lat, loc.lng);
+      this.showMarker(loc.lat, loc.lng);
+      setTimeout(() => {
+        
+        
+        this.map.invalidateSize();
+      }, 1200); // kleiner Delay nötig bei Ionic
+    } else {
+      console.error('Location nicht verfügbar');
+    }
+  }
+
+
+  showMap(lat: number, lng: number) {
+    console.log('starting to show map ...');
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
+      iconUrl: 'assets/leaflet/marker-icon.png',
+      shadowUrl: 'assets/leaflet/marker-shadow.png',
+    });
+
+    if (this.map) {
+      this.map.remove(); // alte Instanz entfernen
+    }
+
+    this.map = L.map('map').setView([lat, lng], 13); // z. B. München
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(this.map);
+
+
+
+/*
+    this.map.on('click', (e: L.LeafletMouseEvent) => {
+      const { lat, lng } = e.latlng;
+
+      this.showMarker(lat, lng);
+    });
+    */
+  }
+
+
+  showMarker(lat: number, lng: number) {
+    console.log('starting to show marker ...');
+    // Marker platzieren oder verschieben
+    if (this.marker) {
+      this.marker.setLatLng([lat, lng]);
+    } else {
+      this.marker = L.marker([lat, lng], { draggable: true }).addTo(this.map);
+
+      this.marker.on('dragend', (event) => {
+        const newLatLng = this.marker.getLatLng();
+        console.log('Marker verschoben:', newLatLng.lat, newLatLng.lng);
+
+        // Optional: Weiterverarbeiten
+     //   this.onMarkerMoved(newLatLng.lat, newLatLng.lng);
+      });
+    }
+  }
+
+
+  async onMarkerMoved(lat: number, lng: number) {
+    console.log('Ausgewählter Standort:', lat, lng);
+    this.map.setView(new L.LatLng(lat, lng), this.map.getZoom());
+  }
+
+
+  async refreshMap() {
+
+  }
+
 }
